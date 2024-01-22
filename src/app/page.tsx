c3e260a43377bf0bf62f343422dd3e4da62d43d0
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-22 20:54:57 +0300

    chore: hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div>
+        <h1 className='text-2xl'>Hello world</h1>
+        <button className='btn btn-primary w-full'>Start</button>
+      </div>
+    </div>
+  );
 }
