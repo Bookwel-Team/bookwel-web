332cc700d663062998fdc9b73d0e583dd2cedce0
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-22 18:34:50 +0300

    chore: initialize app theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#F3BB3C',
+          secondary: '#080E46',
+          accent: '#F3BB3C',
+          neutral: '#F3F3F2',
+          'base-100': '#f0ffff',
+          info: '#00ccff',
+          success: '#aded00',
+          warning: '#b24100',
+          error: '#da0000',
+        },
+        dark: {
+          ...require('daisyui/src/theming/themes')['dark'],
+          primary: '#080E46',
+          secondary: '#F3BB3C',
+          accent: '#080E46',
+          neutral: '#F3F3F2',
+          'base-100': '#f0ffff',
+          info: '#00ccff',
+          success: '#aded00',
+          warning: '#b24100',
+          error: '#da0000',
+        },
       },
-    },
+    ],
+    darkTheme: 'dark',
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: '',
+    logs: true,
+    themeRoot: ':root',
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
