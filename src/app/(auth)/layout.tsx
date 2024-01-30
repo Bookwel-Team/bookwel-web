4d2c5ddb7fff57d658543fdfa6533594a0fee792
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-30 08:17:10 +0300

    feat: implement login interface

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
+import LOGIN_LOGO from '@/assets/login-logo.png';
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='w-full h-full flex justify-center items-center'>
       <div className='w-full h-full sm:w-3/4 lg:w-2/3 xl:w-1/2 sm:h-2/3 bg-white rounded-2xl p-3 flex justify-around items-center'>
         <div className='basis-1/2 relative flex justify-center items-center'>
-          <div className='w-[15rem] h-[15rem] rounded-full bg-neutral flex justify-center items-center relative p-8'></div>
+          <Image src={LOGIN_LOGO} alt='logo' />
         </div>
         <div className='basis-1/2 flex justify-center items-center'>{children}</div>
       </div>
