eead5e6b881fbdb60f7704cfc00c0aef715cf0c2
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-30 17:15:00 +0300

    chore: redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+'use client';
+
+import { LOGIN_PAGE } from '@/utilities/constants';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { BsTriangle } from 'react-icons/bs';
+
 export default function Home() {
+  const route = useRouter();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      route.push(LOGIN_PAGE);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, 2000);
+  }, [route]);
+
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div>
-        <h1 className='text-2xl'>Hello world</h1>
-        <button className='btn btn-primary w-full'>Start</button>
+        <span className='animate-spin'>
+          <BsTriangle className='m-2 -translate-y-[1px] text-primary' size={24} />
+        </span>
       </div>
     </div>
   );
--- src/utilities/constants/path.ts
+++ src/utilities/constants/path.ts
@@ -1,1 +1,2 @@
 export const HOME_PAGE = '/home';
+export const LOGIN_PAGE = '/signin';
