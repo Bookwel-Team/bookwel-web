eb42d8b2cce506190bf6fac19aba7f2ed5b82e46
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-09 00:39:41 +0300

    style: reformat code

--- src/utilities/context/auth-context/auth-context.ts
+++ src/utilities/context/auth-context/auth-context.ts
@@ -1,6 +1,6 @@
-import { createContext } from "react";
-import { TAuthStore } from "./types";
-import { User } from "firebase/auth";
+import { createContext } from 'react';
+import { TAuthStore } from './types';
+import { User } from 'firebase/auth';
 
 export const AuthContext = createContext<TAuthStore>({
   user: {} as User,
