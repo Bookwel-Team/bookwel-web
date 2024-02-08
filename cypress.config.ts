137ac4a26e56da4d3fd8e8cd2cac1f1930acd857
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-08 11:38:05 +0300

    chore: config cypress

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'cypress';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: ['cypress/**/*'],
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    specPattern: 'src/**/*.cy.{ts,tsx,js,jsx}',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+    baseUrl: 'http://localhost:5173/',
+  },
+});
