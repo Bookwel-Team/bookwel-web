6f26b9704fc29530dc9e8e107ad93171fe784953
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-08 21:06:03 +0300

    fix-up: make auth layout responsive

--- src/utilities/Layouts/AuthLayout.tsx
+++ src/utilities/Layouts/AuthLayout.tsx
@@ -6,10 +6,10 @@ export const AuthLayout = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full h-full sm:w-3/4 lg:w-2/3 xl:w-1/2 sm:h-2/3 bg-white rounded-2xl p-3 flex justify-around items-center'>
-        <div className='basis-1/2 relative flex justify-center items-center'>
+        <div className='basis-0 lg:basis-1/2 relative flex justify-center items-center'>
           <LazyLoadImage src={LOGO} alt='logo' />
         </div>
-        <div className='basis-1/2 flex justify-center items-center'>
+        <div className='basis-full lg:basis-1/2 flex justify-center items-center'>
           <Outlet />
         </div>
       </div>
