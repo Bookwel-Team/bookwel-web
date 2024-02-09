c5bfac023ec8b3fb5c89089c570c97caabb32d25
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2024-02-09 11:11:58 +0300

    chore: configure interaction user api provider

--- src/providers/type.ts
+++ src/providers/type.ts
@@ -1,4 +1,5 @@
-import { authProvider } from '.';
+import { authProvider, userProvider } from '.';
 
 export type TSingUp = typeof authProvider.signUp;
 export type TSingIn = typeof authProvider.signIn;
+export type TCreateUser = typeof userProvider.createUser;
