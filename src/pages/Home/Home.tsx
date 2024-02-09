fec21011d65d59d88798934ec28f4e0e4bd8606c
Rojotiana Nomena <hei.nomena@gmail.com>
2024-02-09 17:42:06 +0300

    feat: implement chat bot

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,26 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { LOGIN_PAGE } from '../../utilities/constants';
-import { BsTriangle } from 'react-icons/bs';
+import { BsRobot as BsRobotIcon } from 'react-icons/bs';
 import './Home.css';
 import Logo from '../../assets/logo-nobg.png';
-import { GoSearch as GoSearchIcon } from 'react-icons/go';
 import Navbar from './components/Navbar';
+import { ChangeEvent, useState } from 'react';
 
 export const Home = () => {
-  // const navigate = useNavigate();
+  const [message, setMessage] = useState('');
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     navigate(LOGIN_PAGE);
-  //     return () => {
-  //       clearTimeout(timeout);
-  //     };
-  //   }, 2000);
-  // }, [navigate]);
+  const handleClick = e => {
+    try {
+      e.preventDefault();
+      if (message) {
+        // Chat api handler
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
 
   return (
     <div className='w-screen'>
@@ -30,16 +33,24 @@ export const Home = () => {
             </div>
             <h1 className='text-7xl mb-4'>BookKwell</h1>
             <div className='mb-4'>
-              <p>Welcome to BookWel, your personalized book recommendation app!</p>
+              <p>Welcome to BookKwell, your personalized book recommendation app!</p>
               <p>Tired of wandering aimlessly in literary landscapes?</p>
-              <p>BookWel takes the guesswork out of finding your next captivating read. </p>
+              <p>BookKwell takes the guesswork out of finding your next captivating read. </p>
               <p>Powered by intelligent algorithms and curated by bibliophiles like you, it's your own personal librarian in your pocket.</p>
             </div>
             <div className='w-full relative'>
-              <input type='text' placeholder='Search book' className='input input-bordered bg-white w-full text-gray-500 rounded-full' />
-              <span className='absolute top-1/2 -translate-y-1/2 right-1 text-white bg-secondary p-2 rounded-full shadow-lg'>
-                <GoSearchIcon size={24} />
-              </span>
+              <form onSubmit={handleClick}>
+                <input
+                  type='text'
+                  onChange={handleChange}
+                  placeholder='Message Chat...'
+                  className='input input-bordered bg-white w-full text-gray-500 rounded-full'
+                  value={message}
+                />
+                <span className='absolute top-1/2 -translate-y-1/2 right-1 text-white bg-secondary p-2 rounded-full shadow-lg'>
+                  <BsRobotIcon onClick={handleClick} size={24} />
+                </span>
+              </form>
             </div>
           </div>
         </div>
