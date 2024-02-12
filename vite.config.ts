3bb84d15f2f136648ab382d71b4e51aa5491e379
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-12 17:21:00 +0300

    infra: remove component test for testing

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
     istanbul({
       cypress: true,
       requireEnv: false,
+      nycrcPath: './.nycrc.json',
       forceBuildInstrument: true,
+      include: './src/*',
+      extension: ['.ts', '.tsx'],
     }),
   ],
   optimizeDeps: {
