9fa41fa7cdc0b3377da9d3d104af62910b6c4278
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-12 09:15:07 +0300

    test: fix category test

--- cypress/e2e/Category.cy.tsx
+++ cypress/e2e/Category.cy.tsx
@@ -1,40 +1,31 @@
-const category1 = {id: "testId1", name: "nameTest1"};
-const category2 = {id: "testId2", name: "nameTest2"};
+const category1 = { id: 'testId1', name: 'nameTest1' };
+const category2 = { id: 'testId2', name: 'nameTest2' };
 
 describe('Test Categories', () => {
-    it('Test categories interface and validation', () => {
-        cy.visit('/categories');
-        cy.intercept('GET', '/categories', (request) => {
-            request.reply([
-                category1,
-                category2
-            ])
-        })
-        cy.intercept('GET', "/categories/testId1/creaction", (request)=> {
-            request.reply({})
-        })
-        cy.intercept('GET', "/categories/testId2/creaction", (request)=> {
-            request.reply({})
-        })
+  it('Test categories interface and validation', () => {
+    cy.visit('/categories');
+    cy.intercept('GET', '/categories', [category1, category2]);
+    cy.intercept('GET', '/categories/testId1/reaction', {});
+    cy.intercept('GET', '/categories/testId2/reaction', {});
 
-        cy.dataCy('nextButton').should("exist")
+    cy.dataCy('nextButton').should('exist');
 
-        const buttonCat1 =  cy.contains(category1.name)
+    const buttonCat1 = cy.contains(category1.name);
 
-        cy.contains("What are your interests ?")
-        cy.contains(category1.name)
-        cy.contains(category2.name)
+    cy.contains('What are your interests ?');
+    cy.contains(category1.name);
+    cy.contains(category2.name);
 
-        cy.nameCy('categorySearch').type('{enter}');
-        cy.nameCy('categorySearch').type(category1.name);
+    cy.nameCy('categorySearch').type('{enter}');
+    cy.nameCy('categorySearch').type(category1.name);
 
-        //buttonCat1.should("have.class", "dark:hover:text-white")
-        buttonCat1.click()
-        buttonCat1.should("have.class", "dark:hover:bg-neutral")
+    //buttonCat1.should("have.class", "dark:hover:text-white")
+    buttonCat1.click();
+    buttonCat1.should('have.class', 'dark:hover:bg-neutral');
 
-        cy.contains(category1.name)
-        cy.should('not.contain.text', category2.name)
+    cy.contains(category1.name);
+    cy.should('not.contain.text', category2.name);
 
-        cy.dataCy('nextButton').click()
-    })
-})
+    cy.dataCy('nextButton').click();
+  });
+});
