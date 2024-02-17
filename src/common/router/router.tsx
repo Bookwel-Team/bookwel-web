d4a50a69953caf3e3c4a3d7b11ceee47c866b016
Rojotiana Nomena <hei.nomena@gmail.com>
2024-02-17 19:45:48 +0300

    fix: chatbot path

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Books, Home, SignIn, SignUp, CategoryPage, NotFound, Profile } from '../../pages';
+import { Books, Chatbot, SignIn, SignUp, CategoryPage, NotFound, Profile, Home } from '../../pages';
 import { AuthLayout, NeedAuth } from '../Layouts';
 
 export const AppRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
   {
     path: '/',
     element: <NeedAuth />,
     errorElement: <NotFound />,
     children: [
       {
-        path: '/',
-        element: <Home />,
+        path: '/chat',
+        element: <Chatbot />,
       },
       {
         path: '/books',
