c38a4c8f2e0a3d1f01fd2726577473cb6228be39
Onitsiky <hei.onitsiky@gmail.com>
2024-02-17 23:07:41 +0300

    fix: adding reaction on a book

--- src/common/components/book-card-cover/Card.tsx
+++ src/common/components/book-card-cover/Card.tsx
@@ -1,36 +1,35 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { FC, useEffect } from 'react';
-import { FaRegHeart } from 'react-icons/fa';
+import { FC, useState } from 'react';
+import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa';
 import { GiFeather } from 'react-icons/gi';
 import { GoDownload } from 'react-icons/go';
 import { LuBook } from 'react-icons/lu';
 import { BookCardCoverProps } from './type';
 import { useFetch } from '../../hooks';
 import { reactionProvider } from '../../../providers/reaction-provider';
-import { BookReaction, ReactionStatus } from '@onitsiky/bookwel-typescript-client';
-import { TDownloadBook, TGetBookReaction, TReactToBook } from '../../../providers';
+import { BookReaction, CrupdateReaction, ReactionStatus } from '@onitsiky/bookwel-typescript-client';
+import { TDownloadBook, TReactToBook } from '../../../providers';
 import { BsTriangle } from 'react-icons/bs';
 import { bookProvider } from '../../../providers/book-provider';
 
 export const BookCardCover: FC<BookCardCoverProps> = ({ book, needReaction = true, userId }) => {
   const { fetch: reactToBook } = useFetch<BookReaction, TReactToBook>(reactionProvider.reactToBook);
-  const { fetch: getBookReaction, data: reaction, isLoading: reactionLoading } = useFetch<BookReaction, TGetBookReaction>(reactionProvider.getBookReaction);
   const { fetch: downloadBook, isLoading: downloadBookLoading } = useFetch<void, TDownloadBook>(bookProvider.downloadBook);
+  const [isReactionLoading, setIsReactionLoading] = useState(false);
 
-  useEffect(() => {
-    if (needReaction && userId && book.id) {
-      getBookReaction(book.id, userId);
-    }
-  }, []);
-
-  const handleReact = () => {
-    console.log(needReaction, userId, book.id);
+  const handleReact = (reaction: ReactionStatus) => () => {
+    setIsReactionLoading(true);
+    console.log(needReaction, "user " + userId, "book " + book.id);
     if (needReaction && userId && book.id) {
       const bookId = book.id;
-      reactToBook(bookId, userId, reaction?.reactionStatus === ReactionStatus.LIKE ? ReactionStatus.UNSET : ReactionStatus.LIKE).then(() => {
-        getBookReaction(bookId, userId);
+      const data : CrupdateReaction = {
+        reactorId: userId,
+        reactionStatus: reaction
+      }
+      reactToBook(bookId, data).then(() => {
       });
     }
+    setIsReactionLoading(false);
   };
 
   const handleDownload = () => {
@@ -57,16 +56,23 @@ export const BookCardCover: FC<BookCardCoverProps> = ({ book, needReaction = tru
             data-cy='download-button'
             className='active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2  text-white bg-primary p-2 rounded-full shadow-lg'
           >
-            {downloadBookLoading ? <BsTriangle className='animate-spin text-white' size={24} /> : <GoDownload size={24} />}
+            {downloadBookLoading ? <BsTriangle className='animate-spin text-white' size={24} /> :
+              <GoDownload size={24} />}
           </span>
-          {needReaction && (
-            <span
-              onClick={handleReact}
-              className={`relative active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2  text-white ${reaction?.reactionStatus === ReactionStatus.LIKE ? 'bg-error' : 'bg-primary'} p-2 rounded-full shadow-lg`}
-            >
-              {reactionLoading ? <BsTriangle className='animate-spin text-white' size={24} /> : <FaRegHeart data-cy='reaction-button' size={24} />}
+          <span
+            onClick={handleReact(ReactionStatus.LIKE)}
+            className={`relative active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2  text-white ${book.reactionStatistics?.byCurrentUser === ReactionStatus.LIKE ? 'bg-green-600' : 'bg-primary'} p-2 rounded-full shadow-lg`}
+          >
+              {isReactionLoading ? <BsTriangle className='animate-spin text-white' size={24} /> :
+                <FaThumbsUp data-cy='reaction-button' size={24} />}
             </span>
-          )}
+          <span
+            onClick={handleReact(ReactionStatus.DISLIKE)}
+            className={`relative active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2  text-white ${book.reactionStatistics?.byCurrentUser === ReactionStatus.LIKE ? 'bg-error' : 'bg-primary'} p-2 rounded-full shadow-lg`}
+          >
+              {isReactionLoading ? <BsTriangle className='animate-spin text-white' size={24} /> :
+                <FaThumbsDown data-cy='reaction-button' size={24} />}
+            </span>
         </div>
       </div>
     </div>
