dff7e1925b49208b12ec18a02be9d67f1c06d2ed
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2024-02-18 16:46:18 +0300

    test: upload book e2e test

---
+++ cypress/e2e/UploadBook.cy.tsx
@@ -0,0 +1,17 @@
+import { categoryList } from "./mock/category";
+
+describe('Test Upload book', () => {
+    it('Test upload book interface and validation', () => {
+        cy.visit('/upload-book');
+        cy.intercept('GET', '/categories', categoryList);
+        cy.intercept('POST', '/books', {})
+
+        cy.dataCy('input-picture').selectFile("cypress/fixtures/assets/picture.jpg")
+        cy.dataCy('input-pdf').selectFile("cypress/fixtures/assets/book-one.pdf")
+        cy.dataCy('autocomplete-show').click()
+        cy.contains(categoryList[0].name).click()
+
+        cy.dataCy('submit-book').click()
+        cy.contains('Book added succesfully.')
+    })
+})
---
+++ cypress/e2e/mock/category.ts
@@ -0,0 +1,4 @@
+export const categoryList = [
+    { id: 'testId1', name: 'nameTest1' },
+    { id: 'testId2', name: 'nameTest2' }
+]
