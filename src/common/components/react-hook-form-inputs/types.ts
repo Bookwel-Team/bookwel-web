7df808388e9d2d0bae592921584e178b61108713
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2024-02-18 16:39:16 +0300

    reformat: autocomplete

--- src/common/components/react-hook-form-inputs/types.ts
+++ src/common/components/react-hook-form-inputs/types.ts
@@ -13,3 +13,22 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export type PasswordInputProps = Omit<InputProps, 'endIcon' | 'toggleVisibility' | 'type'>;
+
+export interface InputFileProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  accept: string;
+  label: string;
+  hideLabel?: boolean;
+  hideAll?: boolean;
+  containerClassName?: string;
+  getFileChange?: (newFile: File) => void;
+}
+
+export interface InputAutocomplete {
+  items: string[];
+  name: string;
+  loadItems: boolean;
+  label: string;
+  onChange(val: string): void;
+  resetValue(): void;
+}
