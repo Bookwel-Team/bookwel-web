2a0443165a0c8c39bfd9908ea29f1ca5f41e3444
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2024-02-18 16:31:33 +0300

    feat: upload book resolver

--- src/common/resolvers/field-error-messages.ts
+++ src/common/resolvers/field-error-messages.ts
@@ -1,4 +1,7 @@
 export const fieldErrorMessages = {
   password_not_match: 'Passwords do not match. Please retry.',
   password_too_sort: 'Password should have seven (7) character at least.',
+  category_not_match: 'Category is mandatory.',
+  file_not_match: 'File is mandatory.',
+  file_not_supported: (type: string) => `File not supported. Type ${type} only is accepted.`,
 };
