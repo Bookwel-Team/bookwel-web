1a43ae9c652a92227f961881a912bc9d5db206b7
Fanomezana Sarobidy RAKOTOMAHEFA <hei.sarobidy@gmail.com>
2024-02-18 16:47:09 +0300

    refactor(test): category mock

--- cypress/e2e/Category.cy.tsx
+++ cypress/e2e/Category.cy.tsx
@@ -1,30 +1,29 @@
-const category1 = { id: 'testId1', name: 'nameTest1' };
-const category2 = { id: 'testId2', name: 'nameTest2' };
+import { categoryList } from "./mock/category";
 
 describe('Test Categories', () => {
   it('Test categories interface and validation', () => {
     cy.visit('/categories');
-    cy.intercept('GET', '/categories', [category1, category2]);
+    cy.intercept('GET', '/categories', categoryList);
     cy.intercept('GET', '/categories/testId1/reaction', {});
     cy.intercept('GET', '/categories/testId2/reaction', {});
 
     cy.dataCy('nextButton').should('exist');
 
-    const buttonCat1 = cy.contains(category1.name);
+    const buttonCat1 = cy.contains(categoryList[0].name);
 
     cy.contains('What are your interests ?');
-    cy.contains(category1.name);
-    cy.contains(category2.name);
+    cy.contains(categoryList[0].name);
+    cy.contains(categoryList[1].name);
 
     cy.nameCy('categorySearch').type('{enter}');
-    cy.nameCy('categorySearch').type(category1.name);
+    cy.nameCy('categorySearch').type(categoryList[0].name);
 
     //buttonCat1.should("have.class", "dark:hover:text-white")
     buttonCat1.click();
     buttonCat1.should('have.class', 'dark:hover:bg-neutral');
 
-    cy.contains(category1.name);
-    cy.should('not.contain.text', category2.name);
+    cy.contains(categoryList[0].name);
+    cy.should('not.contain.text', categoryList[1].name);
 
     cy.dataCy('nextButton').click();
   });
